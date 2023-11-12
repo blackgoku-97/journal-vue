ac6b803723ea8786b9a0b6c5d95b44adbeb69e96
Ricardo <rpe19971210@gmail.com>
2023-11-11 21:25:36 -0300

    Primer commit

---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,23 @@
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: 'Culpa minim deserunt occaecat pariatur quis id sit incididunt. Ut exercitation et laboris cupidatat. Enim sit est aliquip cupidatat sunt tempor.',
+            picture: null
+        },
+        {
+            id: new Date().getTime() + 1000,
+            date: new Date().toDateString(),
+            text: 'Elit ex esse commodo veniam dolore. Culpa pariatur dolor consectetur voluptate ea laborum id dolore incididunt aute esse. Officia occaecat ea veniam esse anim cillum excepteur ea esse aliqua Lorem velit ut. Adipisicing ad sint aliqua sunt irure exercitation ea ullamco ex. Cillum ea laboris sit tempor duis consectetur aute. Lorem quis sint do adipisicing eu cillum Lorem incididunt officia magna non eu irure. Reprehenderit duis enim dolor nulla voluptate eiusmod sunt ad dolore officia exercitation.',
+            picture: null
+        },
+        {
+            id: new Date().getTime() + 2000,
+            date: new Date().toDateString(),
+            text: 'Pariatur consectetur anim ea laboris adipisicing do nulla. Et elit dolore labore labore et cupidatat nulla fugiat anim enim labore. Commodo qui do laborum in eiusmod sit magna. Do excepteur ea laborum dolor voluptate duis ut esse aute non dolor.',
+            picture: null
+        },
+    ]
+})
---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <h1>Hola Mundo</h1>
+    
+    <button @click="goToDaybook" class="btn btn-primary">Primary</button>
+    <button @click="goToDaybook" class="btn btn-secondary">Secondary</button>
+    <button @click="goToDaybook" class="btn btn-success">Success</button>
+
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    goToDaybook() {
+      this.$router.push({ name: 'no-entry' })
+    }
+  }
+}
+</script>
